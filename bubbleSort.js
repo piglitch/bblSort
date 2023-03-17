b5645fc79a56e1dc6d54e5bed3ac6a60bc3fab47
piglitch <73684837+piglitch@users.noreply.github.com>
2023-03-17 21:38:07 +0530

    Add files via upload

---
+++ bubbleSort.js
@@ -0,0 +1,16 @@
+function bubblesort(arr){
+    for (let i = 0; i < arr.length-1; i++){
+        for (let j = 0; j < arr.length - i - 1; j++){
+            if (arr[j] > arr[j+1]){
+               let temp  = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            } 
+        }
+    }
+return arr;  
+}
+
+let arr = [8,5,0,9,8,5,2,3,8,8] 
+
+console.log(bubblesort(arr))
